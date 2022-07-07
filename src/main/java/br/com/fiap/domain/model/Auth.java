223633ac87b3c2870c93943c161199a7bd00c410
João Vitor Almeida <jvitor.almeida98@gmail.com>
2022-07-06 21:09:59 -0300

    changes

--- src/main/java/br/com/fiap/domain/model/Auth.java
+++ src/main/java/br/com/fiap/domain/model/Auth.java
@@ -5,10 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Auth {
     @JsonProperty("access_token")
     private String accessToken;
+
+    public Auth(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
