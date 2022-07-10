1547ec58a13c94dafb1a4e89489f6d53406299c3
João Vitor Almeida <jvitor.almeida98@gmail.com>
2022-07-10 17:19:36 -0300

    teste

--- src/main/java/br/com/fiap/domain/model/User.java
+++ src/main/java/br/com/fiap/domain/model/User.java
@@ -6,12 +6,14 @@ import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.smallrye.common.constraint.NotNull;
 import org.bson.codecs.pojo.annotations.BsonIgnore;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @MongoEntity(collection = "user")
 public class User  {
+    public ObjectId id;
     public String name;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     public LocalDate birthDate;
