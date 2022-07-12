f11edb72d794e465036e8784f8df9783c7bdbe54
João Vitor <jvitor.almeida98@gmail.com>
2022-07-12 07:59:36 -0300

    date to string

--- src/main/java/br/com/fiap/domain/model/User.java
+++ src/main/java/br/com/fiap/domain/model/User.java
@@ -1,22 +1,14 @@
 package br.com.fiap.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import io.quarkus.mongodb.panache.PanacheMongoEntity;
-import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import io.smallrye.common.constraint.NotNull;
 import org.bson.codecs.pojo.annotations.BsonIgnore;
 import org.bson.types.ObjectId;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
 @MongoEntity(collection = "user")
-public class User  {
+public class User {
     public ObjectId id;
     public String name;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    public LocalDate birthDate;
+    public String birthDate;
     public String email;
     public Long cellphone;
     public String cpf;
